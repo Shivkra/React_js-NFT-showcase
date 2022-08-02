6329795a4d642ef00bd3fcfec818da61af101465
sivasankarkk <109886104+sivasankarkk@users.noreply.github.com>
2022-08-02 06:28:35 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -35,7 +35,7 @@ const App = () => {
 
       <div className="px-4 py-2 justify-center items-center bg-primary flex-col text-center banner04">
         <p className={`${styles.pText} ${styles.whiteText}`}>Made with love by {" "}
-        <span className="bold">JavaScript Mastery</span>
+        <span className="bold">Shivkra</span>
         </p>
       </div>
     </>
